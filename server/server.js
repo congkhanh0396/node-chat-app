8b2f2c0af9822d0855b2be70687e9bd88f56f57e
congkhanh0396 <congkhanh0396@gmail.com>
2018-03-15 19:40:15 +0700

    Greet new users and alert other

--- server/server.js
+++ server/server.js
@@ -13,13 +13,27 @@ var io = socketIO(server);
 io.on('connect',(socket)=>{
     console.log("New user connected");
     
+    socket.emit('newMessage',{
+        from: 'Admin',
+        text: 'Wellcome to the chat app',
+        createAt: new Date().getTime()
+    })
+
+    socket.broadcast.emit('newMessage',{
+        from: 'Admin',
+        text: 'A new user joined',
+        createAt: new Date().getTime()
+    });
+
     socket.on('createMsg',(message) => {
         console.log('createMsg', message);
-        io.emit('newMessage',{
+
+        socket.broadcast.emit('newMessage',{
             from: message.from,
             text: message.text,
             createAt: new Date().getTime()
         });
+
     });
 
     socket.on('disconnect',()=>{
